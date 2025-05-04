ac70a64e87137d19b3ace6d6f99def2856f6b11f
Panagiota.tr <panayiota03@gmail.com>
2025-05-04 23:42:03 +0300

    xml files

--- app/src/main/java/com/example/healthup/Contacts/EditContactsActivity.java
+++ app/src/main/java/com/example/healthup/Contacts/EditContactsActivity.java
@@ -1,6 +1,7 @@
 package com.example.healthup.Contacts;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +10,21 @@ import com.example.healthup.R;
 
 public class EditContactsActivity extends AppCompatActivity {
 
+    EditText editName, editPhone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_edit_contacts);
+
+        editName = findViewById(R.id.editNameContact);
+        editPhone = findViewById(R.id.editPhoneContact);
+
+
+        String name = getIntent().getStringExtra("name");
+        String phone = getIntent().getStringExtra("phone");
+
+        editName.setText(name);
+        editPhone.setText(phone);
     }
 }
