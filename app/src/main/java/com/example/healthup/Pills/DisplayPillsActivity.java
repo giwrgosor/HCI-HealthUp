6b165056b34189fa85d3e1959e8340666856a360
Panagiota.tr <panayiota03@gmail.com>
2025-05-05 19:20:57 +0300

    create files for pills

---
+++ app/src/main/java/com/example/healthup/Pills/DisplayPillsActivity.java
@@ -0,0 +1,20 @@
+package com.example.healthup.Pills;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.healthup.R;
+
+public class DisplayPillsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_pills);
+    }
+}
