c130629e14a46484a2d08cb517fcebe197ff9bfc
george_sorogkas <georgesor1@hotmail.com>
2025-05-25 19:51:42 +0300

    Emergency voice reco complete

--- app/src/main/java/com/example/healthup/Sos/EmergencySelectionActivity.java
+++ app/src/main/java/com/example/healthup/Sos/EmergencySelectionActivity.java
@@ -1,6 +1,7 @@
 package com.example.healthup.Sos;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,11 +11,13 @@ import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -26,8 +29,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.healthup.MainMenuActivity;
+import com.example.healthup.MemoryDAO.UserMemoryDAO;
 import com.example.healthup.R;
+import com.example.healthup.dao.UserDAO;
 import com.example.healthup.domain.EmergencyCallTracker;
+import com.example.healthup.domain.HttpRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class EmergencySelectionActivity extends AppCompatActivity {
 
@@ -37,6 +48,7 @@ public class EmergencySelectionActivity extends AppCompatActivity {
     private EmergencyCallTracker callTracker;
     private ImageButton homeButtonEmergency;
     private ImageButton voiceEmergencySelection_btn;
+    private String voiceText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,5 +122,63 @@ public class EmergencySelectionActivity extends AppCompatActivity {
         Intent callIntent = new Intent(Intent.ACTION_CALL);
         callIntent.setData(Uri.parse(phoneNumber));
         startActivity(callIntent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        int REQUEST_SPEECH_RECOGNIZER = 3000;
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("DEMO-REQUESTCODE",
+                Integer.toString(requestCode));
+        Log.i("DEMO-RESULTCODE", Integer.toString(resultCode));
+        if (requestCode == REQUEST_SPEECH_RECOGNIZER && resultCode == Activity.RESULT_OK && data != null) {
+            ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            voiceText = text.get(0);
+
+            UserDAO userDAO = new UserMemoryDAO();
+            String url = userDAO.getUrl() + "/soscall";
+
+            try {
+                JSONObject json = new JSONObject();
+                json.put("text", voiceText);
+
+                HttpRequest.sendPostRequest(url, json.toString(), new HttpRequest.ResponseListener() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject json = new JSONObject(response);
+                            String service = json.getString("service");
+                            service = service.equals("null") ? "" : service;
+                            if(service.equalsIgnoreCase("ambulance")){
+                                callEmergency("tel:166");
+                            }else if(service.equalsIgnoreCase("police")){
+                                callEmergency("tel:100");
+                            }else if(service.equalsIgnoreCase("firedepartment")){
+                                callEmergency("tel:199");
+                            }else{
+                                handleError("We could not understand your request. Please try again.");
+                            }
+                        } catch (JSONException e) {
+                            Log.e("HTTP_RESPONSE", "JSON parsing error: " + e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("Error", error);
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            System.out.println("Recognizer API error");
+        }
+    }
+
+    public void handleError(String msg){
+        Toast.makeText(EmergencySelectionActivity.this,msg,Toast.LENGTH_LONG).show();
     }
 }
